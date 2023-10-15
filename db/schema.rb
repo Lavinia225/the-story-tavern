e5e36ea1e4a8036804237af16974fd66fabcbda2
Luna Seifert <thepuddingdeity@gmail.com>
2023-10-15 15:32:55 -0500

    Defaulted access level to 0 in User model, wrote several user tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_15_190752) do
+ActiveRecord::Schema.define(version: 2023_10_15_202536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2023_10_15_190752) do
     t.string "display_name"
     t.string "password_digest"
     t.string "email"
-    t.integer "access_level"
+    t.integer "access_level", default: 0
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "created_at", precision: 6, null: false
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,33 @@ RSpec.describe User, type: :model do
     it 'has a password field' do
         expect(User.new).to respond_to(:password)
     end
+
+    it 'has a username field' do
+        expect(User.new).to respond_to(:username)
+    end
+
+    it 'has a display_name field' do
+        expect(User.new).to respond_to(:display_name)
+    end
+
+    it 'has an email field' do
+        expect(User.new).to respond_to(:email)
+    end
+
+    it 'has an access_level' do
+        expect(User.new).to respond_to(:access_level)
+    end
+
+    it 'defaults access_level to 0' do
+        user = User.new
+        expect(user.access_level).to be 0
+    end
+
+    it 'has a reset_password_token field' do
+        expect(User.new).to respond_to(:reset_password_token)
+    end
+
+    it 'has a reset_password_sent_at field' do
+        expect(User.new).to respond_to(:reset_password_sent_at)
+    end
 end
