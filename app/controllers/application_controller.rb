e54c2eeb72fc47223f49276f7370972b90df6e91
Luna Seifert <thepuddingdeity@gmail.com>
2023-10-20 18:50:13 -0500

    Added Faker, and added authorization check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  before_action :authorize
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
   private
+  
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: {errors: ["Not Authorized!"]}, status: unauthorized unless @current_user
+  end
 
   def render_unprocessable_entity e
     render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
