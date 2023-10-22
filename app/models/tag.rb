b04728cecd0914fbeb6212ba5e9954cedae2cd80
Luna Seifert <thepuddingdeity@gmail.com>
2023-10-22 17:01:44 -0500

    Added a validator to ensure tag is unique

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,12 @@ class Tag < ActiveRecord::Base
 
    validates :story_id, presence: true
    validates :genre_id, presence: true
+   validate :unique
+
+   def unique
+      tag_doesnt_exist = !Tag.find_by(["story_id = ? AND genre_id = ?", "#{self.story_id}", "#{self.genre_id}"])
+      unless tag_doesnt_exist
+         errors.add(:unique, "Must not already exist") 
+      end
+   end
 end
