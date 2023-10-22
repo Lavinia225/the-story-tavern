c7e16e5c2b25da0d17a141688952f09100dbf204
Luna Seifert <thepuddingdeity@gmail.com>
2023-10-21 21:28:13 -0500

    Created Tag Controller/Routes, built create

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,6 @@
 class GenresController < ApplicationController
+    skip_before_action :authorize, only: :index
+    
     def index
         render json: Genre.all
     end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :stories
   resources :genres
+  resources :tags, only: [:create, :destroy]
   
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
