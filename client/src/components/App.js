7dbbdac0cbbd5cefca6402545d5158779f6925c5
Luna Seifert <thepuddingdeity@gmail.com>
2023-10-26 20:44:57 -0500

    Setup the frontend properly

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Haiya! Everything he is still being built in the background.</p>
+        <p>Haiya! Everything is still being built in the background.</p>
       </header>
     </div>
   );
