0632bce9a1de57ef6eeffdd905227f0fb858ff29
Luna Seifert <thepuddingdeity@gmail.com>
2023-10-28 21:37:06 -0500

    Created the Controlled Form to login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,21 @@
+import {Switch, Route} from 'react-router-dom'
+import UserBar from './user_related/UserBar';
+import LoginForm from './user_related/LoginForm';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Haiya! Everything is still being built in the background.</p>
-      </header>
+      <UserBar />
+      <Switch>
+        <Route exact path='/'>
+          <header className="App-header">
+            <p>Haiya! If by some miracle you have obtained the url to this website, know that it is still being created.</p>
+          </header>
+        </Route>
+        <Route path='/login'>
+          <LoginForm />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- client/src/components/user_related/LoginForm.js
+++ client/src/components/user_related/LoginForm.js
@@ -1,8 +1,30 @@
-import {useContext} from 'react'
+import {useState, useContext} from 'react'
 import {UserContext} from '../context/user'
 
 function LoginForm(){
     const {setUser} = useContext(UserContext)
+    const [formData, setFormData] = useState({
+        username: "",
+        password: ""
+    })
+
+    function handleChange(e){
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return(
+    <div>
+        <form id='login-form'>
+            <label htmlFor='username-field'>Username: </label>
+            <input id='username-field' type='text' name='username' value={formData.username} onChange={handleChange} />
+            <label htmlFor='password-field'>Password: </label>
+            <input id='password-field' type='password' name='password' value={formData.password} onChange={handleChange} />
+        </form>
+    </div>
+    )
 }
 
 export default LoginForm
