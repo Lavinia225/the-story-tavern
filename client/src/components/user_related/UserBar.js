c47ef4d99475f1d2c6e9413c77c85be28097d130
Luna Seifert <thepuddingdeity@gmail.com>
2023-10-30 16:33:03 -0500

    Added a logout function and conditional rendering based on login status

--- client/src/components/user_related/UserBar.js
+++ client/src/components/user_related/UserBar.js
@@ -3,11 +3,33 @@ import { UserContext } from "../context/user"
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min"
 
 function UserBar(){
-    const {user} = useContext(UserContext)
+    const {user, setUser} = useContext(UserContext)
+
+    function renderWelcomeLogout(){
+        return(
+            <>
+                <p>Welcome, {user.display_name}</p>
+                <button onClick={logout}>Logout</button>
+            </>
+        )
+    }
+
+    function renderLoginCreateAccount(){
+        return(
+            <>
+                <NavLink to='/login'>Login</NavLink>
+                <NavLink to='/signup'>Create Account</NavLink>
+            </>)
+    }
+
+    function logout(){
+        fetch('/logout')
+        .then(setUser({id: 0, display_name: "", access_level: 0}))
+    }
     
     return(
         <nav id='user-bar'>
-            <NavLink to='/login'>Login</NavLink>
+            {user.id === 0 ? renderLoginCreateAccount() : renderWelcomeLogout()}
         </nav>
     )
 }
