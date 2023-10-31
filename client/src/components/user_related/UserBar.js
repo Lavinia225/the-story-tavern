d9a3179f40de1e25335c54072e7311cb7470ca59
Luna Seifert <thepuddingdeity@gmail.com>
2023-10-30 23:08:15 -0500

    Added delete account functionality

--- client/src/components/user_related/UserBar.js
+++ client/src/components/user_related/UserBar.js
@@ -12,6 +12,7 @@ function UserBar(){
                 <div className='dropdown'>
                     <button>Profile</button>
                     <div className='dropdown-content'>
+                        <a onClick={deleteAccount}>Delete Account</a>
                         <a onClick={logout}>Logout</a>
                     </div>
                 </div>
@@ -25,6 +26,22 @@ function UserBar(){
                 <NavLink to='/login'>Login</NavLink>
                 <NavLink to='/signup'>Create Account</NavLink>
             </>)
+    }
+
+    async function deleteAccount(){
+        const confirmation = window.confirm("Are you completely sure? Accounts are not recoverable.")
+
+        if(confirmation){
+            const response = await fetch(`/users/${user.id}`, {method: "DELETE"})
+
+            if (response.status == 204){
+                setUser({id: 0, display_name: "", access_level: 0})
+            }
+            else{
+                const data = response.json()
+                window.alert("The following has gone horribly wrong: ", data.errors)
+            }
+        }
     }
 
     function logout(){
