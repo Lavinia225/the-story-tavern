3d4cf56a6f5bfcceb5235b46e5a94da6df77da88
Luna Seifert <thepuddingdeity@gmail.com>
2023-11-01 21:18:22 -0500

    Added basic creation form for story

--- client/src/components/story_related/NewStoryForm.js
+++ client/src/components/story_related/NewStoryForm.js
@@ -1,8 +1,24 @@
+import {useState, useContext} from 'react'
+
 function NewStoryForm(){
+    const [formData, setFormData] = useState({
+        title: "",
+        body: ""
+    })
+
+    function handleChange(e){
+        setFormData({...formData, [e.target.id]: e.target.value})
+    }
+
     return(
-    <>
-        <p>hi</p>
-    </>
+    <div>
+        <form id='new-story'>
+            <label htmlFor='title'>Title: </label>
+            <input type='text' id='title' onChange={handleChange} value={formData.title} />
+            <label htmlFor='body'>Body: </label>
+            <input type='textarea' id='body' onChange={handleChange} value={formData.body} />
+        </form>
+    </div>
     )
 }
 
