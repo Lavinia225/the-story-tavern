cc08515dab5830786c4acff4b06932b308308542
Luna Seifert <thepuddingdeity@gmail.com>
2023-11-02 17:37:02 -0500

    Added genre display for the story

--- client/src/components/story_related/Story.js
+++ client/src/components/story_related/Story.js
@@ -2,6 +2,7 @@ import {useState, useEffect, useContext} from 'react'
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
 import { UserContext } from '../context/user'
 import { ErrorsContext } from '../context/errors'
+import StoryTable from './StoryTable'
 
 function Story(){
     const params = useParams()
@@ -25,11 +26,25 @@ function Story(){
         }
     }, [])
 
+    function genreStringer(){
+        let genreString = "Genres: "
+        story.genres.forEach(genre =>{
+            genreString += `${genre.genre}, `
+        })
+        
+        return genreString.slice(0, genreString.length - 2)
+    }
+
+    if (Object.keys(story).length < 1){
+        return <p>Loading</p>
+    }
+
     return(
         <div id='story'>
             {displayErrors()}
             <h2>{story.title}</h2>
             <p id='story-author'>By: {story.user}</p>
+            {story.genres.length > 0 ? <p>{genreStringer()}</p> : null}
             <p id='story-body'>{story.body}</p>
         </div>
     )
