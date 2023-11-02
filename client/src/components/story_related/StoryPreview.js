d1af4770f1a5b2adb8e3f528e95c4b762439d577
Luna Seifert <thepuddingdeity@gmail.com>
2023-11-01 19:16:42 -0500

    Story preview completed.

--- client/src/components/story_related/StoryPreview.js
+++ client/src/components/story_related/StoryPreview.js
@@ -1,7 +1,17 @@
 function StoryPreview({story}){
     return(
-        <>
-        </>
+        <tr id='story-preview'>
+            <td>
+                <p id='story-preview-title'>{story.title}<br/><span id='story-preview-user'>By: {story.user}</span></p>
+                <p id='story-preview-summary'>{story.summary}</p>
+            </td>
+            <td>
+                <ul>
+                    {story.genres.map(genre => <li>{genre.genre}</li>)}
+                </ul>
+            </td>
+            <td>{story.formatted_updated_at}</td>
+        </tr>
     )
 }
 
