e56927b7ae8c930f480f862110266efa7556e806
Luna Seifert <thepuddingdeity@gmail.com>
2023-11-01 21:54:45 -0500

    Added part of genre selection code

--- client/src/components/story_related/NewStoryForm.js
+++ client/src/components/story_related/NewStoryForm.js
@@ -1,10 +1,29 @@
-import {useState, useContext} from 'react'
+import {useState, useEffect, useContext} from 'react'
+import { ErrorsContext } from '../context/errors'
 
 function NewStoryForm(){
+    const [genres, setGenres] = useState([])
+    const {setErrors, displayErrors} = useContext(ErrorsContext)
     const [formData, setFormData] = useState({
         title: "",
         body: ""
     })
+
+    useEffect(()=>{
+        fetchGenres()
+
+        async function fetchGenres(){
+            const res = await fetch('/genres')
+            const data = await res.json()
+            
+            if (res.ok){
+                setGenres(data)
+            }
+            else{
+                setErrors(data.errors)
+            }
+        }
+    }, [])
 
     function handleChange(e){
         setFormData({...formData, [e.target.id]: e.target.value})
@@ -12,6 +31,7 @@ function NewStoryForm(){
 
     return(
     <div>
+        {displayErrors()}
         <form id='new-story'>
             <label htmlFor='title'>Title: </label>
             <input type='text' id='title' onChange={handleChange} value={formData.title} />
