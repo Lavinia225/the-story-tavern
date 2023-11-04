9d09bb688bb8929e553a7d86b2d3eea578aaf537
Luna Seifert <thepuddingdeity@gmail.com>
2023-11-04 17:50:54 -0500

    Wrote part of code to check for user's specific emotes and realized test user needs to have an emote first, so the fetch request will be made next instead of manually creating this data.

--- client/src/components/story_related/EmoteBar.js
+++ client/src/components/story_related/EmoteBar.js
@@ -5,33 +5,50 @@ import { ErrorsContext } from "../context/errors"
 function EmoteBar({emotes}){
     const {user} = useContext(UserContext)
     const {setErrors} = useContext(ErrorsContext)
+    const [loaded, setLoaded] = useState(false)
     const [emoteMap, setEmoteMap] = useState({
         happy: 0,
         sad: 0,
         mad : 0,
         heart: 0
     })
+    const [userEmote, setUserEmote] = useState({
+        happy: false,
+        sad: false,
+        mad: false,
+        heart: false
+    })
 
     useEffect(()=>{
-        const tempEmoteMap = {...emoteMap}
-
-        for(let i = 0; i < emotes.length; i++){
-            const {happy, sad, mad, heart} = emotes[i]
-
-            if (happy === true) tempEmoteMap.happy += 1
-            if (sad === true) tempEmoteMap.sad += 1
-            if (mad === true) tempEmoteMap.mad += 1
-            if (heart === true) tempEmoteMap.heart += 1
+        if (!loaded){
+            populateEmoteMap()
         }
-        setEmoteMap(tempEmoteMap)
+        if (emotes.find(emote =>emote.user_id === user.id)){
+            console.log("clear!")
+        }
+
+        function populateEmoteMap(){
+            const tempEmoteMap = {...emoteMap}
+
+            for(let i = 0; i < emotes.length; i++){
+                const {happy, sad, mad, heart} = emotes[i]
+
+                if (happy === true) tempEmoteMap.happy += 1
+                if (sad === true) tempEmoteMap.sad += 1
+                if (mad === true) tempEmoteMap.mad += 1
+                if (heart === true) tempEmoteMap.heart += 1
+            }
+            setEmoteMap(tempEmoteMap)
+            setLoaded(true)
+        }
     }, [])
 
     return(
         <div>
-            <p>😀 {emoteMap.happy}</p>
-            <p>😢 {emoteMap.sad}</p>
-            <p>😠 {emoteMap.mad}</p>
-            <p>❤️ {emoteMap.heart}</p>
+            <p className={userEmote.happy === true ? "active" : "inactive"}>😀 {emoteMap.happy}</p>
+            <p className={userEmote.sad === true ? "active" : "inactive"}>😢 {emoteMap.sad}</p>
+            <p className={userEmote.mad === true ? "active" : "inactive"}>😠 {emoteMap.mad}</p>
+            <p className={userEmote.heart === true ? "active" : "inactive"}>❤️ {emoteMap.heart}</p>
         </div>
     )
 }
