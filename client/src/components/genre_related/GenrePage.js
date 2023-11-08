fcb0eeffbb68ff4e686ad55d7178cde9271ff76f
Luna Seifert <thepuddingdeity@gmail.com>
2023-11-07 20:08:59 -0600

    Added delete functionality and improved useEffect code)

--- client/src/components/genre_related/GenrePage.js
+++ client/src/components/genre_related/GenrePage.js
@@ -1,29 +1,48 @@
 import {useState, useEffect, useContext} from 'react'
-import { useParams } from "react-router-dom/cjs/react-router-dom.min"
+import { useParams, useHistory } from "react-router-dom/cjs/react-router-dom.min"
 import { GenresContext } from '../context/genres'
 import { ErrorsContext } from '../context/errors'
 
 function GenrePage(){
     const params = useParams()
-    const {genres} = useContext(GenresContext)
+    const history = useHistory()
+    const {genres, setGenres} = useContext(GenresContext)
     const {setErrors, displayErrors} = useContext(ErrorsContext)
-    const [genre, setGenre] = useState("")
+    const [genre, setGenre] = useState({genre: "", id: 0})
     const [editing, setEditing] = useState(false)
-    const [initialRender, setInitialRender] = useState(true)
 
     useEffect(()=>{
-        if (!initialRender){
-            const targetGenre = genres.find(genre =>genre.id === parseInt(params.id)).genre
-            setGenre(targetGenre)
+        if (genres.length > 0){
+            const targetGenre = genres.find(genre =>genre.id === parseInt(params.id))
+            if (targetGenre){
+                setGenre(targetGenre)
+            }
         }
-        setInitialRender(false)
     }, [genres])
 
+    async function handleDelete(){
+        const confirmation = window.confirm("Are you certain?")
+
+        if (confirmation){
+            const response = await fetch(`/genres/${genre.id}`, {method: "DELETE"})
+
+            if (response.ok){
+                setGenres(genres.filter(oldGenre => oldGenre.id !== genre.id))
+                history.goBack()
+            }
+            else{
+                const data = await response.json()
+                setErrors(data.errors)
+            }
+        }
+    }
+
     return(
-        <div>
-            <p>{genre}</p>
+        <div id='genrepage'>
+            {displayErrors()}
+            <p>{genre.genre}</p>
             <button>Edit</button>
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     )
 }
