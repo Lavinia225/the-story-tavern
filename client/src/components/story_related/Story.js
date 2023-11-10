f6489039de30227074cdbf6a8cb11bf8459f13dc
Luna Seifert <thepuddingdeity@gmail.com>
2023-11-09 21:04:10 -0600

    Added delete functionality...

--- client/src/components/story_related/Story.js
+++ client/src/components/story_related/Story.js
@@ -1,12 +1,13 @@
 import {useState, useEffect, useContext} from 'react'
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import { useParams, useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 import { UserContext } from '../context/user'
 import { ErrorsContext } from '../context/errors'
 import EmoteBar from './EmoteBar'
 
 function Story(){
     const params = useParams()
-    const {user} = useContext(UserContext) //For checking if edit/deleting is allowed
+    const history= useHistory()
+    const {user} = useContext(UserContext)
     const {setErrors, displayErrors} = useContext(ErrorsContext)
     const [story, setStory] = useState({})
 
@@ -35,18 +36,49 @@ function Story(){
         return genreString.slice(0, genreString.length - 2)
     }
 
+    async function handleDelete(){
+        const confirmation = window.confirm("Are you confident in this irreversible action?")
+
+        if (confirmation){
+            const res = await fetch(`/stories/${story.id}`, {method: "DELETE"})
+
+            if (res.ok){
+                history.push('/stories')
+            }
+            else{
+                const data = await res.json()
+                setErrors(data.errors)
+            }
+        }
+    }
+
+    function deletePrivilages(){
+        return user.id === story.user_id || user.access_level > 0
+    }
+
     if (Object.keys(story).length < 1){
-        return <p>Loading</p>
+        return (
+            <>
+                {displayErrors()}
+                <p>Loading</p>
+            </>)
     }
 
     return(
         <div id='story'>
             {displayErrors()}
-            <h2>{story.title}</h2>
-            <p id='story-author'>By: {story.user}</p>
+            <h2>{story.title}
+                {deletePrivilages() ?
+                 <span>
+                    {user.id === story.user_id ? <button>Edit</button> : null}
+                    <button onClick={handleDelete}>Delete</button>
+                 </span>
+                : null}
+            </h2>
+            <p id='story-author'>By: {story.creator}</p>
             {story.genres.length > 0 ? <p>{genreStringer()}</p> : null}
             <p id='story-body'>{story.body}</p>
-            <EmoteBar emotes={story.emotes} storyId={story.id}/>
+            {/*<EmoteBar emotes={story.emotes} storyId={story.id}/>*/}
         </div>
     )
 }
