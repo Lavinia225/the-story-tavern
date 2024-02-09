fd0f51fa83c0d1b9f47528f7619fb2a5cbc05a12
Luna Seifert <thepuddingdeity@gmail.com>
2024-02-08 19:05:19 -0600

    Styled dashboard and gave it an initial structure to follow.

--- client/src/components/user_related/SettingsDashboard.js
+++ client/src/components/user_related/SettingsDashboard.js
@@ -1,7 +1,12 @@
 function SettingsDashboard(){
     return(
-        <ul>
-            hi
+        <ul className='user-settings'>
+            <li className='category-box'>
+                <h3>Change Display Name</h3>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" width="16" height="16" focusable="false">
+                    <path d="M9.41 14l-1.29-1.29L11.83 9H2V7h9.83L8.12 3.29 9.41 2l5.29 5.29a1 1 0 010 1.41z"></path>
+                </svg>
+            </li>
         </ul>
     )
 }
