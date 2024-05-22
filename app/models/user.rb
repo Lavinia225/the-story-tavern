af6de6d31477fdf322c86ff8c27a18bfffa18948
Lavinia225 <thepuddingdeity@gmail.com>
2024-05-21 22:14:45 -0500

    Amended validation error message.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
     validates :password, {length: {in: 8..40}}
 
     validates_format_of :email, {multiline: true, with: email_regex}
-    validates_format_of :password, {with: /[a-z]/, message: "a"}
+    validates_format_of :password, {with: /[a-z]/, message: "Password must include a lowercase character"}
     validates_format_of :password, {with: /[A-Z]/, message: "Password must include an uppercase character"}
     validates_format_of :password, {with: /\W/, message: "Password must include a symbol"}
     validates_format_of :password, {with: /\d/, message: "Password must include a number"}
